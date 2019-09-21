e83823ab64d37f1bea89bec8767fe8d0c313eb6e
micki <github@com-science.de>
2019-09-21 23:15:12 +0200

    Idiomatic way of creating thread with a back channel to signal threads when to quit work

--- create-threads.c
+++ create-threads.c
@@ -7,86 +7,98 @@
 #include <pthread.h>
 #include <string.h>
 #include <errno.h>
-
-typedef struct {
-    int fds[2];
-} channel;
+#include <stdbool.h>
 
 static const char msg[] = "Please terminate!";
 
-void* task(void* argv) {
-    channel* ch = (channel*) argv;
-    uint8_t buf[32];
+bool shouldTaskContinue(int channel)
+{
+    char buf[32];
+    size_t n = read(channel, buf, sizeof(buf));
+    switch (n)
+    {
+    case -1:
+        switch (errno)
+        {
+        case EWOULDBLOCK:
+            break;
+        default:
+            perror("read() failed");
+            return false;
+            break;
+        }
+        break;
+    case 0:
+        break;
+    default:
+        buf[n] = '\0';
+        fprintf(stderr, "received: %s\n", buf);
+        if (strcmp(buf, msg) == 0)
+        {
+            return false;
+        }
+        break;
+    }
+    return true;
+}
+
+void *task(void *argv)
+{
+    const char answer[] = "Terminating";
+    int *channel = (int *)argv;
 
     assert(argv);
 
-    for(;;) {
-        fprintf(stderr, "call read()\n");
-        size_t n = read(ch->fds[0], buf, 32);
-        fprintf(stderr, "read return code: %ld\n", n);
-        switch(n) {
-        case -1:
-            switch(errno) {
-            case EWOULDBLOCK:
-                fprintf(stderr, "EWOULDBLOCK\n");
-                break;
-            default:
-                perror("read() failed");
-                break;
-            }
-            break;
-        case 0:
-            break;
-        default:
-            buf[n] = '\0';
-            fprintf(stderr, "received: %s\n", buf);
-            if (strcmp(buf, msg)==0) {
-                goto exitloop;
-            }
-            break;
-        }
-        usleep(rand()+1000);
+    while (shouldTaskContinue(*channel))
+    {
+        int sleeptime = (rand() + 100000) % 1000000;
+        fprintf(stderr, "sleep for %dms\n", sleeptime / 1000);
+        usleep(sleeptime);
     }
-exitloop:
-    fprintf(stderr, "Task finished\n");
+    close(*channel);
+    fprintf(stderr, "terminating\n");
+    return NULL;
 }
 
-channel* startThread( void* (*task)(void*) ) {
+int startThread(void *(*task)(void *))
+{
     pthread_t tid;
     int rc;
-    channel* ch = (channel*) malloc(sizeof(channel));
+    int *ch = (int *)malloc(sizeof(int) * 2);
 
-    rc = pipe(ch->fds);
-    assert(rc==0);
+    rc = pipe(ch);
+    assert(rc == 0);
 
-    rc = fcntl(ch->fds[0], F_SETFL, fcntl(ch->fds[0], F_GETFL) | O_NONBLOCK);
-    assert(rc==0);
+    rc = fcntl(ch[0], F_SETFL, fcntl(ch[0], F_GETFL) | O_NONBLOCK);
+    assert(rc == 0);
 
-    rc = pthread_create(&tid, NULL, task, ch);
-    assert(rc==0);
+    rc = pthread_create(&tid, NULL, task, &ch[0]);
+    assert(rc == 0);
 
     rc = pthread_detach(tid);
-    assert(rc==0);
+    assert(rc == 0);
 
-    return ch;
+    return ch[1];
 }
 
-int main() {
+int main()
+{
     int i;
-    const int numThreads = 1 ;
-    channel* channels[numThreads];
+    const int numThreads = 32;
+    int channels[numThreads];
 
     srand(time(NULL));
-    channels[0] = startThread(task);
+
+    for (i = 0; i < numThreads; ++i)
+        channels[i] = startThread(task);
 
     sleep(2);
-    write(channels[0]->fds[1], msg, sizeof(msg));
-
-    /*
-    for(i=0;i<numThreads;++i) {
-        channels[i] = startThread
+    for (i = 0; i < numThreads; ++i)
+    {
+        write(channels[i], msg, sizeof(msg));
+        close(channels[i]);
     }
-    */
+
     pthread_exit(NULL);
     return 0;
 }
