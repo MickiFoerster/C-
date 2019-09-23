d1173c712927ab7855e7a7a8aba4875fb58afd35
micki <github@com-science.de>
2019-09-23 23:00:05 +0200

    Add IO multiplexing examples

---
+++ io-multiplexing/poll.c
@@ -0,0 +1,22 @@
+for (i=0;i<5;i++) {
+    memset(&client, 0, sizeof (client));
+    addrlen = sizeof(client);
+    pollfds[i].fd = accept(sockfd,(struct sockaddr*)&client, &addrlen);
+    pollfds[i].events = POLLIN;
+}
+
+sleep(1);
+
+while(1) {
+  	puts("round again");
+	poll(pollfds, 5, 50000);
+ 
+	for(i=0;i<5;i++) {
+		if (pollfds[i].revents & POLLIN){
+			pollfds[i].revents = 0;
+			memset(buffer,0,MAXBUF);
+			read(pollfds[i].fd, buffer, MAXBUF);
+			puts(buffer);
+		}
+	}
+}
---
+++ io-multiplexing/select.c
@@ -0,0 +1,35 @@
+/*
+Select – summary:
+
+We need to build each set before each call
+The function check any bit up to the higher number – O(n)
+We need to iterate over the file descriptors to check if it exists on the set returned from select
+The main advantage of select is the fact that it is very portable – every unix like OS has it
+*/
+
+  for (i=0;i<5;i++) 
+  {
+    memset(&client, 0, sizeof (client));
+    addrlen = sizeof(client);
+    fds[i] = accept(sockfd,(struct sockaddr*)&client, &addrlen);
+    if(fds[i] > max)
+    	max = fds[i];
+  }
+  
+  while(1){
+	FD_ZERO(&rset);
+  	for (i = 0; i< 5; i++ ) {
+  		FD_SET(fds[i],&rset);
+  	}
+ 
+   	puts("round again");
+	select(max+1, &rset, NULL, NULL, NULL);
+ 
+	for(i=0;i<5;i++) {
+		if (FD_ISSET(fds[i], &rset)){
+			memset(buffer,0,MAXBUF);
+			read(fds[i], buffer, MAXBUF);
+			puts(buffer);
+		}
+	}	
+  }
