a72c2a8f357a6cba519d22148c50cbfb66cd5b7f
micki <github@com-science.de>
2019-09-25 22:05:42 +0200

    Add changes

--- io-multiplexing/main.c
+++ io-multiplexing/main.c
@@ -1,85 +1,87 @@
-#include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
-#include <netinet/in.h>
-#include <wait.h>
-#include <signal.h>
+#include <arpa/inet.h>
 #include <errno.h>
+#include <netinet/in.h>
+#include <signal.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include <sys/select.h>
+#include <sys/socket.h>
 #include <sys/time.h>
 #include <sys/types.h>
-#include <sys/socket.h>
-#include <arpa/inet.h>
 #include <unistd.h>
- 
+#include <wait.h>
+
 #define MAXBUF 256
 #define SELECT 1
 #define POLL 2
 
 #if IOMULTIPLEXING == POLL
 #include <poll.h>
+#elif IOMULTIPLEXING == EPOLL
+#include <sys/epoll.h>
 #endif
 
-void child_process(void)
-{
+void child_process(void) {
   sleep(2);
   char msg[MAXBUF];
   struct sockaddr_in addr = {0};
-  int n, sockfd,num=1;
+  int n, sockfd, num = 1;
   srandom(getpid());
   /* Create socket and connect to server */
   sockfd = socket(AF_INET, SOCK_STREAM, 0);
   addr.sin_family = AF_INET;
   addr.sin_port = htons(2000);
   addr.sin_addr.s_addr = inet_addr("127.0.0.1");
- 
-  connect(sockfd, (struct sockaddr*)&addr, sizeof(addr));
- 
+
+  connect(sockfd, (struct sockaddr *)&addr, sizeof(addr));
+
   printf("child {%d} connected \n", getpid());
-  while(1){
-        int sl = (random() % 10 ) +  1;
-        num++;
-     	sleep(sl);
-  	sprintf (msg, "Test message %d from client %d", num, getpid());
-  	n = write(sockfd, msg, strlen(msg));	/* Send message */
+  while (1) {
+    int sl = (random() % 10) + 1;
+    num++;
+    sleep(sl);
+    sprintf(msg, "Test message %d from client %d", num, getpid());
+    n = write(sockfd, msg, strlen(msg)); /* Send message */
   }
- 
 }
- 
-int main()
-{
+
+int main() {
   char buffer[MAXBUF];
   int fds[5];
+#if IOMULTIPLEXING == POLL
   struct pollfd pollfds[5];
+#endif
   struct sockaddr_in addr;
   struct sockaddr_in client;
-  int i,max=0;
+  int i, max = 0;
   socklen_t addrlen;
   int sockfd;
   fd_set rset;
-  for(i=0;i<5;i++)
-  {
-  	if(fork() == 0)
-  	{
-  		child_process();
-  		exit(0);
-  	}
+  for (i = 0; i < 5; i++) {
+    if (fork() == 0) {
+      child_process();
+      exit(0);
+    }
   }
- 
+
   sockfd = socket(AF_INET, SOCK_STREAM, 0);
-  memset(&addr, 0, sizeof (addr));
+  memset(&addr, 0, sizeof(addr));
   addr.sin_family = AF_INET;
   addr.sin_port = htons(2000);
   addr.sin_addr.s_addr = INADDR_ANY;
-  bind(sockfd,(struct sockaddr*)&addr ,sizeof(addr));
-  listen (sockfd, 5); 
+  bind(sockfd, (struct sockaddr *)&addr, sizeof(addr));
+  listen(sockfd, 5);
 
 #if IOMULTIPLEXING == SELECT
   fprintf(stderr, "select\n");
-#  include "select.c" 
+#include "select.c"
 #elif IOMULTIPLEXING == POLL
   fprintf(stderr, "poll\n");
-#  include "poll.c" 
+#include "poll.c"
+#elif IOMULTIPLEXING == EPOLL
+  fprintf(stderr, "epoll\n");
+#include "epoll.c"
 #endif
 
   return 0;
--- io-multiplexing/poll.c
+++ io-multiplexing/poll.c
@@ -1,20 +1,40 @@
-for (i=0;i<5;i++) {
-    memset(&client, 0, sizeof (client));
-    addrlen = sizeof(client);
-    pollfds[i].fd = accept(sockfd,(struct sockaddr*)&client, &addrlen);
-    pollfds[i].events = POLLIN;
+/*
+Poll vs Select
+* poll( ) does not require that the user calculate the value of the highest-numbered 
+  file descriptor +1. poll( ) is more efficient for large-valued file descriptors. 
+  Imagine watching a single file descriptor with the value 900 via select() - the 
+  kernel would have to check each bit of each passed-in set, up to the 900th bit.
+* select( )’s file descriptor sets are statically sized.
+* With select( ), the file descriptor sets are reconstructed on return, so each 
+  subsequent call must reinitialize them. The poll() system call separates the input
+  (events field) from the output (revents field), allowing the array to be reused 
+  without change.
+* The timeout parameter to select() is undefined on return. Portable code needs to 
+  reinitialize it. This is not an issue with pselect()
+* select() is more portable, as some Unix systems do not support poll()
+*/
+
+for (i = 0; i < 5; i++)
+{
+	memset(&client, 0, sizeof(client));
+	addrlen = sizeof(client);
+	pollfds[i].fd = accept(sockfd, (struct sockaddr *)&client, &addrlen);
+	pollfds[i].events = POLLIN;
 }
 
 sleep(1);
 
-while(1) {
-  	puts("round again");
+while (1)
+{
+	puts("round again");
 	poll(pollfds, 5, 50000);
- 
-	for(i=0;i<5;i++) {
-		if (pollfds[i].revents & POLLIN){
+
+	for (i = 0; i < 5; i++)
+	{
+		if (pollfds[i].revents & POLLIN)
+		{
 			pollfds[i].revents = 0;
-			memset(buffer,0,MAXBUF);
+			memset(buffer, 0, MAXBUF);
 			read(pollfds[i].fd, buffer, MAXBUF);
 			puts(buffer);
 		}
